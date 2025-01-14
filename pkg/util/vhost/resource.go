01e3a5077fbf72708b420a046f197b72c885e418
LxHTT <lxhtt@mcsl.com.cn>
2025-01-15 00:18:24 +0800

    delete: img

--- pkg/util/vhost/resource.go
+++ pkg/util/vhost/resource.go
@@ -19,6 +19,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/fatedier/frp/pkg/util/log"
 	"github.com/fatedier/frp/pkg/util/version"
@@ -32,13 +33,12 @@ const (
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<title>镜缘映射 · ME Frp</title>
+<title>503 Service Unavailable | ME Frp</title>
 <style>
 body{font-family:Arial,sans-serif;background-color:#f4f4f4;margin:0;line-height:1.6;padding:0}
 .container{width:100%;max-width:600px;margin:0 auto;background-color:#fff;padding:20px;box-shadow:0 0 10px rgba(0,0,0,.1);font-size:16px}
 .header{text-align:center;padding:10px 0;display:flex;align-items:center;justify-content:center}
-.header img{height:48px;width:48px;margin-right:10px}
-.header h1{margin:0;font-size:30px}
+.header h2{margin:0;font-size:30px}
 .content{margin:20px 0}
 .footer{text-align:center;color:#888;font-size:12px;margin-top:20px}
 </style>
@@ -46,16 +46,22 @@ body{font-family:Arial,sans-serif;background-color:#f4f4f4;margin:0;line-height:
 <body>
 <div class="container">
 <div class="header">
-<img src="https://img.ltyears.com/editor/23904/20241227/0d0d374e13bb5ea22e17e73a6c49f5e5.svg?t=1735314090679" alt="ME Frp Logo">
-<h1>镜缘映射 · ME Frp</h1>
+<h2>镜缘映射 · ME Frp</h2>
 </div>
 <div class="content">
-<h2>404 - 页面未找到</h2>
-<p>抱歉, 您访问的页面不存在。</p>
-<p>请检查您输入的网址是否正确, 或返回首页。</p>
+<h3>503 - 服务不可用</h3>
+<p>如果您是隧道所有者，造成无法访问的原因可能有：</p>
+<li>您访问的网站使用了内网穿透，但是对应的客户端没有运行。</li>
+<li>该网站或隧道已被管理员临时或永久禁止连接。</li>
+<li>域名解析更改还未生效或解析错误，请检查设置是否正确。</li>
+<li>隧道使用的必须为HTTP或HTTPS类型的隧道</li>
+<p>如果您是普通访问者，您可以：</p>
+<li>稍等一段时间后再次尝试访问此站点。</li>
+<li>尝试与该网站的所有者取得联系。</li>
+<li>刷新您的 DNS 缓存或在其他网络环境访问。</li>
 </div>
 <div class="footer">
-<p>此页面由系统自动生成</p>
+<p>此页面由 ME Frp 服务端自动生成</p>
 <p>&copy; ME Frp 项目组 2021-2025.</p>
 <p>Frp 内网穿透联盟统一识别编码：AZWB66WB</p>
 </div>
@@ -73,7 +79,7 @@ func getNotFoundPageContent() []byte {
 	if NotFoundPagePath != "" {
 		buf, err = os.ReadFile(NotFoundPagePath)
 		if err != nil {
-			log.Warnf("读取自定义 404 页面错误: %v", err)
+			log.Warnf("读取自定义 503 页面错误: %v", err)
 			buf = []byte(NotFound)
 		}
 	} else {
@@ -84,13 +90,13 @@ func getNotFoundPageContent() []byte {
 
 func NotFoundResponse() *http.Response {
 	header := make(http.Header)
-	header.Set("server", "frp/"+version.Full())
+	header.Set("Server", "MEFrp/"+strings.Split(version.Full(), "_")[1])
 	header.Set("Content-Type", "text/html")
 
 	content := getNotFoundPageContent()
 	res := &http.Response{
-		Status:        "Not Found",
-		StatusCode:    404,
+		Status:        "Service Unavailable",
+		StatusCode:    503,
 		Proto:         "HTTP/1.1",
 		ProtoMajor:    1,
 		ProtoMinor:    1,
